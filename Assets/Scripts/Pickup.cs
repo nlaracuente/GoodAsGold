0363809543b0aeec9c1eee0f7f1a3799bd39040b
Nelson Laracuente <nlaracuente09@gmail.com>
2017-12-02 10:21:33 -0700

    adds collectables and curse

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// These are the items the player is trying to avoid
+/// When the player comes in contact with them they are "collected"
+/// and removed from the level
+/// </summary>
+public class Pickup : MonoBehaviour
+{
+    /// <summary>
+    /// Rotation speed
+    /// </summary>
+    [SerializeField]
+    float rotationSpeed = -120f;
+
+    /// <summary>
+    /// How fast to rotate when collected
+    /// </summary>
+    [SerializeField]
+    float pickedupSpeed = 500f;
+
+    /// <summary>
+    /// True when this is picked up
+    /// This is to help prevent the coroutine from starting more than once
+    /// </summary>
+    bool isItemCollected = false;
+
+    /// <summary>
+    /// Since these are non-physic based collectables
+    /// </summary>
+    void Update()
+    {
+        // Rotate
+        Vector3 targetRotation = new Vector3(
+            0f,
+            this.rotationSpeed * Time.deltaTime,
+            0f
+        );
+
+        this.transform.Rotate(targetRotation);
+    }
+
+    /// <summary>
+    /// Triggers the "pickup" logic when the player enters into its triggers
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (this.isItemCollected) {
+            return;
+        }
+
+        if(other.tag == "Player") {
+            StartCoroutine(this.ItemCollected(other.gameObject.GetComponent<Player>()));
+        }
+    }
+
+    /// <summary>
+    /// Increases the player's total pickup
+    /// Destroys itself
+    /// 
+    /// @TODO: play pickup animation
+    /// </summary>
+    IEnumerator ItemCollected(Player player)
+    {
+        yield return null;
+        this.isItemCollected = true;
+        player.CursedItemCollected();
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,31 +5,66 @@ using UnityEngine;
 /// <summary>
 /// Controls player behavior
 /// </summary>
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(MeshRenderer), typeof(Material))]
 public class Player : MonoBehaviour
 {
     /// <summary>
-    /// How fast the player moves
+    /// The fasted the players moves when fully healed
     /// </summary>
     [SerializeField]
-    float moveSpeed = 3f;
+    float maxSpeed = 6f;
 
     /// <summary>
-    /// How fast the player rotates
+    /// The current speed the player moves at
+    /// Defaults to max
+    /// </summary>
+    float moveSpeed;
+
+    /// <summary>
+    /// The fastes the player rotates
     /// </summary>
     [SerializeField]
-    float rotationSpeed = 3f;
+    float maxRotationSpeed = 10f;
+
+    /// <summary>
+    /// The current speed the player rotates at
+    /// Defaults to max
+    /// </summary>
+    float rotationSpeed;
 
     /// <summary>
     /// The angle at which the player can start moving
     /// </summary>
     [SerializeField]
-    float angleDistance;
+    float angleDistance = 0.01f;
+
+    /// <summary>
+    /// Total pickups the player has to collect 
+    /// </summary>
+    [SerializeField]
+    int maxPickups = 5;
+
+    /// <summary>
+    /// How much to reduce the player's movement speed
+    /// When they collect a cursed pickup
+    /// </summary>
+    float speedDamper;
+
+    /// <summary>
+    /// How much to reduce the player's rotational speed
+    /// When they collect a cursed pickup
+    /// </summary>
+    float rotationDamper;
 
     /// <summary>
     /// A reference to the rigidbody component
     /// </summary>
     new Rigidbody rigidbody;
+
+    /// <summary>
+    /// A reference to the Mesh Renderer component
+    /// </summary>
+    new MeshRenderer renderer;
 
     /// <summary>
     /// A reference to the level camera to process movements/rotations based on its position
@@ -39,16 +74,39 @@ public class Player : MonoBehaviour
     /// <summary>
     /// Saves the player's input
     /// </summary>
-    [SerializeField]
     Vector3 inputVector = Vector3.zero;
+
+    /// <summary>
+    /// Total pickups the player has collected
+    /// </summary>
+    [SerializeField]
+    int pickups;
+
+    /// <summary>
+    /// How fast to change color
+    /// </summary>
+    [SerializeField]
+    float colorChangeDelay = 1.5f;
+
+    /// <summary>
+    /// Array of color to change the player into as the curse progresses
+    /// Each index reprents a "state" based on total pickups collected
+    /// </summary>
+    [SerializeField]
+    Color[] cursedColors;
 
     /// <summary>
     /// Initialize
     /// </summary>
     void Awake ()
     {
-        this.rigidbody = GetComponent<Rigidbody>();
-	}
+        this.rigidbody      = GetComponent<Rigidbody>();
+        this.renderer       = GetComponent<MeshRenderer>();
+        this.moveSpeed      = this.maxSpeed;
+        this.rotationSpeed  = this.maxRotationSpeed;
+        this.speedDamper    = this.maxSpeed / this.maxPickups;
+        this.rotationDamper = this.maxRotationSpeed / this.maxPickups;
+    }
 
     /// <summary>
     /// Assigns references to external objects
@@ -104,5 +162,71 @@ public class Player : MonoBehaviour
 
         Vector3 targetPosition = this.rigidbody.position + direction * this.moveSpeed * Time.fixedDeltaTime;
         this.rigidbody.MovePosition(targetPosition);
+    }
+
+    /// <summary>
+    /// Triggers the curse's effect by slowing down the player's movement and rotation
+    /// Changes the the mesh's color to simulate turning into gold
+    /// </summary>
+    public void CursedItemCollected()
+    {
+        this.pickups++;
+
+        this.moveSpeed = Mathf.Max(
+            0,
+            Mathf.Min(this.maxSpeed, this.moveSpeed - this.speedDamper)
+        );
+
+        this.rotationSpeed = Mathf.Max(
+            0,
+            Mathf.Min(this.maxRotationSpeed, this.rotationSpeed - this.rotationDamper)
+        );
+
+        // Fire off the color changer
+        StopCoroutine(this.ChangeColorPerCurse());
+        StartCoroutine(this.ChangeColorPerCurse());
+    }
+
+    /// <summary>
+    /// Gradually changes the color of the player to match their current curse state
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ChangeColorPerCurse()
+    {
+        // Default to current
+        Color newColor = this.renderer.material.color;
+
+        // Update based on curse percentage
+        float cursedPercent = ((float)this.pickups / (float)this.maxPickups) * 100;
+
+        if (cursedPercent >= 25 && cursedPercent < 50) {
+            newColor = this.cursedColors[0];
+        } else if (cursedPercent >= 50 && cursedPercent < 75) {
+            newColor = this.cursedColors[1];
+        } else if (cursedPercent >= 75 & cursedPercent < 100) {
+            newColor = this.cursedColors[2];
+        } else if (cursedPercent >= 100) {
+            newColor = this.cursedColors[3];
+        }
+
+        while (this.renderer.material.color != newColor) {
+            this.renderer.material.color = Color.Lerp(
+            this.renderer.material.color,
+                newColor,
+                this.colorChangeDelay * Time.deltaTime
+            );
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// Removes the effects of being cursed
+    /// Resets speed/rotation back to max
+    /// </summary>
+    void LiftCurse()
+    {
+        this.pickups = 0;
+        this.moveSpeed = this.maxSpeed;
+        this.rotationSpeed = this.maxRotationSpeed;
     }
 }
