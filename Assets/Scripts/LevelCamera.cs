262c8679d1f67f2923046173260f22e009895089
Nelson Laracuente <nlaracuente09@gmail.com>
2017-12-02 07:13:10 -0700

    adds basic player controls based on camera position

---
+++ Assets/Scripts/LevelCamera.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main camera in a level 
+/// Has a focul point and can be rotate -90 or 90 degress
+/// </summary>
+public class LevelCamera : MonoBehaviour
+{
+    /// <summary>
+    /// A reference to the camera component
+    /// </summary>
+    [SerializeField]
+    Camera mainCamera;
+
+    /// <summary>
+    /// Main Level Camera
+    /// </summary>
+    public Camera MainCamera
+    {
+        get { return this.mainCamera; }
+    }
+
+    /// <summary>
+    /// Where to look at and pivot around
+    /// </summary>
+    [SerializeField]
+    Transform targertTransform;
+
+    /// <summary>
+    /// Angles in degree to rotate when a direciton is triggered 
+    /// </summary>
+    [SerializeField]
+    float rotationAngle = 90f;
+
+    /// <summary>
+    /// How fast to rotate around the target
+    /// </summary>
+    [SerializeField]
+    float rotationSpeed = 10f;
+
+    /// <summary>
+    /// The angle to rotate towards
+    /// </summary>
+    [SerializeField]
+    float targetAngle = 0f;
+
+    /// <summary>
+    /// True while the camera is still rotating
+    /// </summary>
+    bool isRotating = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float angle = 0;
+
+        // Left
+        if (Input.GetKey(KeyCode.Q)) {
+            angle = -this.rotationAngle;
+    
+        // Right
+        } else if (Input.GetKey(KeyCode.E)) {
+            angle = this.rotationAngle;
+        }
+
+        // Update rotation
+        if(!this.isRotating && angle != 0) {
+            this.targetAngle = angle;
+            StartCoroutine(this.SmoothRotate());
+        }        
+    }
+
+    /// <summary>
+    /// Continues to rotate towards the target rotation until it reaches 
+    /// a treshold to then snap into place
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator SmoothRotate()
+    {
+        this.isRotating = true;
+
+        while (this.targetAngle != 0) {
+            float speed = this.targetAngle > 0f ? -this.rotationSpeed : this.rotationSpeed;
+            this.transform.RotateAround(this.targertTransform.position, Vector3.up, speed);
+            this.targetAngle += speed;
+            yield return new WaitForEndOfFrame();
+        }
+        
+        this.isRotating = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls player behavior
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// How fast the player moves
+    /// </summary>
+    [SerializeField]
+    float moveSpeed = 3f;
+
+    /// <summary>
+    /// How fast the player rotates
+    /// </summary>
+    [SerializeField]
+    float rotationSpeed = 3f;
+
+    /// <summary>
+    /// The angle at which the player can start moving
+    /// </summary>
+    [SerializeField]
+    float angleDistance;
+
+    /// <summary>
+    /// A reference to the rigidbody component
+    /// </summary>
+    new Rigidbody rigidbody;
+
+    /// <summary>
+    /// A reference to the level camera to process movements/rotations based on its position
+    /// </summary>
+    LevelCamera levelCamera;
+
+    /// <summary>
+    /// Saves the player's input
+    /// </summary>
+    [SerializeField]
+    Vector3 inputVector = Vector3.zero;
+
+    /// <summary>
+    /// Initialize
+    /// </summary>
+    void Awake ()
+    {
+        this.rigidbody = GetComponent<Rigidbody>();
+	}
+
+    /// <summary>
+    /// Assigns references to external objects
+    /// </summary>
+    void Start()
+    {
+        this.levelCamera = FindObjectOfType<LevelCamera>();
+    }
+
+    /// <summary>
+    /// Store player inputs to trigger rotation/movement
+    /// </summary>
+    void Update ()
+    {
+        this.inputVector = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            0f,
+            Input.GetAxisRaw("Vertical")
+        );
+
+        if(this.inputVector.magnitude > 1) {
+            this.inputVector.Normalize();
+        }
+	}
+
+    /// <summary>
+    /// Performs rotations and movements
+    /// </summary>
+    void FixedUpdate()
+    {
+        // Vector3.zero means the player is not moving
+        if (this.inputVector == Vector3.zero) {
+            return;
+        }
+
+        // Translate input vector based on camera position
+        Vector3 direction = this.levelCamera.MainCamera.transform.TransformDirection(this.inputVector);
+        direction.y = 0f;
+        
+        Quaternion targetRotation = Quaternion.Lerp(
+                this.rigidbody.rotation,
+                Quaternion.LookRotation(direction, Vector3.up),
+                this.rotationSpeed * Time.fixedDeltaTime
+        );
+                
+        this.rigidbody.MoveRotation(targetRotation);
+
+        // Wait until rotation is at a certain angle before allowing movement
+        // so that the player does not look awkward trying to moving while facing a different direction
+        if(Quaternion.Angle(this.rigidbody.rotation, targetRotation) > this.angleDistance) {
+            return;
+        }
+
+        Vector3 targetPosition = this.rigidbody.position + direction * this.moveSpeed * Time.fixedDeltaTime;
+        this.rigidbody.MovePosition(targetPosition);
+    }
+}
