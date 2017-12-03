512aa00633f1e20e5b206156699194428cfce6af
Nelson Laracuente <nlaracuente09@gmail.com>
2017-12-02 19:59:36 -0700

    adds exit tile

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Triggers a level completed when the player touches it
+/// </summary>
+public class Exit : MonoBehaviour
+{
+    /// <summary>
+    /// A reference to the level menu
+    /// </summary>
+    LevelMenu menu;
+
+    /// <summary>
+    /// Initialize
+    /// </summary>
+    private void Start()
+    {
+        this.menu = FindObjectOfType<LevelMenu>();
+    }
+
+    /// <summary>
+    /// Triggers a level won
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Player" || this.menu.isMenuOpened) {
+            return;
+        }
+        
+        this.menu.GameWonMenu();
+    }
+}
