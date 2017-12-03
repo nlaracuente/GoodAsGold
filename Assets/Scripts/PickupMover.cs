8c235f9fd5aa2dec928abb5d5aab4e8a751e72e1
Nelson Laracuente <nlaracuente09@gmail.com>
2017-12-02 18:07:03 -0700

    fixes pickup mover logic to ensure they move to target location until finished

--- Assets/Scripts/PickupMover.cs
+++ Assets/Scripts/PickupMover.cs
@@ -11,21 +11,37 @@ using UnityEngine;
 public class PickupMover : MonoBehaviour, ITriggerable
 {
     /// <summary>
-    /// Starting position
-    /// </summary>
-    Vector3 startingPos;
-
-    /// <summary>
     /// Where to go when button is active
     /// </summary>
     [SerializeField]
     Vector3 destinationPos = new Vector3(0f, -3f, 0);
 
     /// <summary>
+    /// Starting position
+    /// </summary>
+    Vector3 startingPos;
+
+    /// <summary>
+    /// The position to move towards
+    /// </summary>
+    Vector3 targetPos;
+
+    /// <summary>
     /// How fast to move
     /// </summary>
     [SerializeField]
     float moveSpeed = 5f;
+
+    /// <summary>
+    /// How close to get to the target before snapping into place
+    /// </summary>
+    [SerializeField]
+    float distanceToTarget = 0.01f;
+
+    /// <summary>
+    /// True while the routine is running
+    /// </summary>
+    bool isRoutineRunning = false;
 
     /// <summary>
     /// Initialize
@@ -40,23 +56,38 @@ public class PickupMover : MonoBehaviour, ITriggerable
             button.RegisterButtonEvents(this);
         }
     }
-    
+
     /// <summary>
-    /// Hides pickups by sinking into the ground
+    /// Fires off a coroutine to move this object to the target position
+    /// As long as there isn't one running already
     /// </summary>
-    public void OnButtonPressed()
+    void Update()
     {
-        StopCoroutine("MoveToPosition");
-        StartCoroutine(this.MoveToPosition(this.destinationPos, this.moveSpeed));
+        // Wait for the coroutine to be done
+        if (this.isRoutineRunning) {
+            return;
+        }
+
+        // Trigger the coroutine
+        if(this.transform.position != this.targetPos) {
+            StartCoroutine(this.MoveToPosition());
+        }
     }
 
     /// <summary>
-    /// Revels pickup by rising from the ground
+    /// Sets the target position to destination position
+    /// </summary>
+    public void OnButtonPressed()
+    {
+        this.targetPos = this.destinationPos;
+    }
+
+    /// <summary>
+    /// Sets the target position to starting position
     /// </summary>
     public void OnButtonReleased()
     {
-        StopCoroutine("MoveToPosition");
-        StartCoroutine(this.MoveToPosition(this.startingPos, this.moveSpeed));
+        this.targetPos = this.startingPos;
     }
 
     /// <summary>
@@ -65,18 +96,20 @@ public class PickupMover : MonoBehaviour, ITriggerable
     /// <param name="destination"></param>
     /// <param name="speed"></param>
     /// <returns></returns>
-    IEnumerator MoveToPosition(Vector3 destination, float speed)
+    IEnumerator MoveToPosition()
     {
-        while(Vector3.Distance(destination, this.transform.position) > 1f) {
+        this.isRoutineRunning = true;
+
+        while(Vector3.Distance(this.targetPos, this.transform.position) > this.distanceToTarget) {
             this.transform.position = Vector3.MoveTowards(
                 this.transform.position,
-                destination,
-                speed * Time.deltaTime
+                this.targetPos,
+                this.moveSpeed * Time.deltaTime
             );
             yield return new WaitForEndOfFrame();
         }
-
-        Debug.Log("Done Moving");
-        this.transform.position = destination;
+        
+        this.transform.position = this.targetPos;
+        this.isRoutineRunning = false;
     }
 }
