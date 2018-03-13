6600415439ed1aa7d6e3058dd5f01cab2862fc83
Nelson Laracuente <nlaracuente09@gmail.com>
2018-03-13 15:18:04 -0700

    adds tiles with moveable objects to the map generation

---
+++ Assets/Scripts/Tiles/MoveableTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moveable tiles are tiles that contain an object that the player can push or pull
+/// </summary>
+public class MoveableTile : BaseTile
+{
+    /// <summary>
+    /// Make the tile match the height of the floor it is on
+    /// </summary>
+    protected override void SpawnComponent()
+    {
+        MatchFloorHeight();
+    }
+}
